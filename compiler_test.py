2d1310ea8ff454c3a91bbe4503050797cd450dfa
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-14 18:40:29 -0300

    Tested all the process of compilation for string literal

--- compiler_test.py
+++ compiler_test.py
@@ -2,6 +2,10 @@
 import unittest
 
 from compiler import tokenizer
+from compiler import parser
+from compiler import transformer
+from compiler import code_generator
+from compiler import compiler
 
 class TestStringLiteralCompilation(unittest.TestCase):
 
@@ -13,8 +17,36 @@ class TestStringLiteralCompilation(unittest.TestCase):
 			{ 'type': 'string', 'value': 'hello world' }
 		]
 
+		self.ast = {
+			'type': 'Program',
+			'body': [{
+				'type': 'StringLiteral',
+				'value': 'hello world',
+			}]
+		}
+
+		self.newAst = {
+			'type': 'Program',
+			'body': [{
+				'type': 'StringLiteral',
+				'value': 'hello world'
+			}]
+		}
+
 	def test_tokenization(self):
 		self.assertEqual(tokenizer(self.input), self.tokens)
+
+	def test_parsing(self):
+		self.assertEqual(parser(self.tokens), self.ast)
+
+	def test_transformation(self):
+		self.assertEqual(transformer(self.ast), self.newAst)
+
+	def test_code_generation(self):
+		self.assertEqual(code_generator(self.newAst), self.output)
+
+	def test_compilation(self):
+		self.assertEqual(compiler(self.input), self.output)
 
 if __name__ == '__main__':
 	unittest.main()
