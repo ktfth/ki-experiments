2d75131ea45a819ee23cff6e9887edea8417535e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-21 14:26:38 -0300

    Covered multiple operations

--- compiler_test.py
+++ compiler_test.py
@@ -307,5 +307,22 @@ class TestSumOperationStmtCompilation(unittest.TestCase):
 	def test_compilation(self):
 		self.assertEqual(compiler(self.input), self.output)
 
+class TestAccSumOperationStmtCompilation(unittest.TestCase):
+
+	def setUp(self):
+		self.input = '1 + 1 + 1'
+		self.output = '1 + 1 + 1'
+
+		self.tokens = [
+			{'type': 'int', 'value': '1'},
+			{'type': 'operation', 'value': '+'},
+			{'type': 'int', 'value': '1'},
+			{'type': 'operation', 'value': '+'},
+			{'type': 'int', 'value': '1'},
+		]
+
+	def test_tokenization(self):
+		self.assertEqual(tokenizer(self.input), self.tokens)
+
 if __name__ == '__main__':
 	unittest.main()
